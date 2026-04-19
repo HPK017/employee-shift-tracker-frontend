1bd3ffaa356948b9d0e1c79dc57d8b7639df1c35
HPK017 <parelharsh0250@gmail.com>
2026-04-19 18:24:04 +0530

    open route for register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './modules/register/register-component/register-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //default
+    {path : '', component: RegisterComponent}
+    
+];
